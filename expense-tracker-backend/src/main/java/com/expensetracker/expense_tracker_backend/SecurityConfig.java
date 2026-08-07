e0ab4b69baaffd55f3e669beca1fcb60a8416dfd
Sarika <sarik04143@gmail.com>
2026-08-07 07:54:34 +0530

    Explicitly permit OPTIONS requests in SecurityConfig

--- expense-tracker-backend/src/main/java/com/expensetracker/expense_tracker_backend/SecurityConfig.java
+++ expense-tracker-backend/src/main/java/com/expensetracker/expense_tracker_backend/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.expensetracker.expense_tracker_backend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -56,6 +57,7 @@ public class SecurityConfig{
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().permitAll()
                 );
